74aadf461540269a840817f157575d8ff50dd96c
Ian Warren <103790584+Duckkk819@users.noreply.github.com>
2023-10-03 23:07:52 -0600

    Got Test UI working

--- ARandomizerModModule.cs
+++ ARandomizerModModule.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using Microsoft.Xna.Framework;
+using Monocle;
 using MonoMod.ModInterop;
 
 namespace Celeste.Mod.ARandomizerMod {
@@ -8,10 +9,12 @@ namespace Celeste.Mod.ARandomizerMod {
         public static ARandomizerModModule Instance { get; private set; }
 
         public override Type SettingsType => typeof(ARandomizerModModuleSettings);
-        public static ARandomizerModModuleSettings Settings => (ARandomizerModModuleSettings) Instance._Settings;
+        public static ARandomizerModModuleSettings Settings => (ARandomizerModModuleSettings)Instance._Settings;
 
         public override Type SessionType => typeof(ARandomizerModModuleSession);
-        public static ARandomizerModModuleSession Session => (ARandomizerModModuleSession) Instance._Session;
+        public static ARandomizerModModuleSession Session => (ARandomizerModModuleSession)Instance._Session;
+
+        UI ui;
 
         public ARandomizerModModule()
         {
@@ -27,24 +30,37 @@ namespace Celeste.Mod.ARandomizerMod {
 
         public override void Load() {
             On.Celeste.Level.TransitionRoutine += TestLoadVariant;
-            On.Celeste.Player.WallJumpCheck += NoWallJump;
+            On.Celeste.Level.Render += LevelRender;
             typeof(ExtendedVariantImports).ModInterop();
-        }
 
-        private bool NoWallJump(On.Celeste.Player.orig_WallJumpCheck orig, Player self, int dir)
-        {
-            return false;
         }
 
         private IEnumerator TestLoadVariant(On.Celeste.Level.orig_TransitionRoutine orig, Level self, LevelData next, Vector2 direction)
         {
-            ExtendedVariantImports.TriggerBooleanVariant.Invoke("AlwaysInvisible", true, false);
+            //ExtendedVariantImports.TriggerBooleanVariant.Invoke("AlwaysInvisible", true, false);
+            //ui.Active = true;
             return orig(self, next, direction);
         }
 
+        private void LevelRender(On.Celeste.Level.orig_Render orig, Level self)
+        {
+            orig(self);
+            if (Engine.Scene is not Level level)
+                return;
+            if (ui == null)
+            {
+                ui = new UI
+                {
+                    Active = true
+                };
+            }
 
-        public override void Unload() {
-            // TODO: unapply any hooks applied in Load()
+            level.Add(ui);
+        }
+
+        public override void Unload()
+        {
+
         }
     }
 }
---
+++ UI.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Monocle;
+using Celeste;
+
+namespace Celeste.Mod.ARandomizerMod
+{
+	public class UI : Entity
+	{
+        public UI()
+        {
+            AddTag(Tags.HUD);
+        }
+
+        public override void Render()
+        {
+            base.Render();
+
+            Draw.Rect(new Vector2(10, 250), 500, 250, new(0, 0, 0, 200));
+            ActiveFont.Draw("Test UI Box", new Vector2(10, 325), Color.White);
+        }
+    }
+}
+
