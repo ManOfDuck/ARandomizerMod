70b5132bd419fca39a94cbed342a8fce6ffcf40a
Ian Warren <103790584+Duckkk819@users.noreply.github.com>
2024-02-11 00:33:03 -0700

    Initial template

---
+++ Source/ARandomizerModModuleSaveData.cs
@@ -0,0 +1,5 @@
+namespace Celeste.Mod.ARandomizerMod {
+    public class ARandomizerModModuleSaveData : EverestModuleSaveData {
+
+    }
+}
---
+++ Source/ARandomizerModModuleSettings.cs
@@ -0,0 +1,5 @@
+namespace Celeste.Mod.ARandomizerMod {
+    public class ARandomizerModModuleSettings : EverestModuleSettings {
+
+    }
+}
