cb3675e5d6f726e6ef7a17986c888a7fb42374b1
Ian Warren <Ian.Warren.Home@gmail.com>
2024-04-23 15:30:49 -0600

    oops

--- Source/Randomizer/VariantManager.cs
+++ Source/Randomizer/VariantManager.cs
@@ -25,9 +25,6 @@ namespace Celeste.Mod.ARandomizerMod
             }
             else
             {
-                TestAllVariants();
-                return;
-
                 // Update active variants
                 RandomizeNewVariants();
 
